6b25ae0f1861452dc7aa282b4bb0361b8eca344b
Pavan Tej <pavan.tej@tamu.edu>
2017-05-14 23:15:59 -0500

    Delete Q3.sql

--- Q3.sql
+++
@@ -1,4 +0,0 @@
-select [Doctor],[Professor],[Singer],[Actor]
-from 
-(select *, row_number() over (partition by Occupation Order by Name) as Rownumber from Occupations) as RN
-PIVOT (max(name) for Occupation in ([Doctor],[Professor],[Singer],[Actor])) as Pivot_table;
